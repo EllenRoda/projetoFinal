bc0593e4f260896c449c97c2ca604c887d619a11
EllenRoda <programacr001@gmail.com>
2025-08-01 15:39:23 -0300

    atualizacao30

--- src/app/component/fornecedor/fornecedor-update/fornecedor-update.component.ts
+++ src/app/component/fornecedor/fornecedor-update/fornecedor-update.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FornecedorService } from '../fornecedor.service';
+import { Fornecedor } from '../fornecedor.model';
 
 @Component({
   selector: 'app-fornecedor-update',
@@ -6,5 +9,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./fornecedor-update.component.css']
 })
 export class FornecedorUpdateComponent {
+fornecedor!: Fornecedor;
 
+  constructor(private fornecedorService: FornecedorService,
+    private router: Router,
+    private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id')
+    this.fornecedorService.readById(id!).subscribe((fornecedor: Fornecedor) =>{
+      this.fornecedor = fornecedor
+    })
+  }
+
+  updateFornecedor(): void {
+    this.fornecedorService.update(this.fornecedor).subscribe(() => {
+      this.fornecedorService.showMessage('Fornecedor atualizado com sucesso!')
+      this.router.navigate(['/fornecedor'])
+    })
+  }
+
+  cancel(): void {
+    this.router.navigate(['/fornecedor'])
+  }
 }
--- src/app/component/fornecedor/fornecedor-visu/fornecedor-visu.component.ts
+++ src/app/component/fornecedor/fornecedor-visu/fornecedor-visu.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Fornecedor } from '../fornecedor.model';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FornecedorService } from '../fornecedor.service';
 
 @Component({
   selector: 'app-fornecedor-visu',
   templateUrl: './fornecedor-visu.component.html',
   styleUrls: ['./fornecedor-visu.component.css']
 })
-export class FornecedorVisuComponent {
+export class FornecedorVisuComponent implements OnInit {
+fornecedor: Fornecedor = {
+  forNomeFantasia: '',
+  forRazaoSocial: '',
+  forCpf: '',
+  forStatus: '',
+  forDataCadastro: null,
+  forResponsavel: '',
 
+  conCelular: '',
+  conTelefoneComercial: '',
+  conEmail: '',
+
+  endRua: '',
+  endNumero: '',
+  endCidade: '',
+  endBairro: '',
+  endCep: '',
+  endEstado: '',
+  endPais: '',
+  endComplemento: '',
+  endTipoResidencia: ''
+}
+
+  constructor(
+    private fornecedorService: FornecedorService,
+    private route: ActivatedRoute,
+    private router: Router // Importa o Router para usar no voltar()
+  ) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      this.fornecedorService.readById(id).subscribe((forn: Fornecedor) => {
+        this.fornecedor = forn;
+      });
+    }
+  }
+
+  voltar(): void {
+    this.router.navigate(['/fornecedor']); // Caminho para voltar à lista de fornecedores
+  }
 }
